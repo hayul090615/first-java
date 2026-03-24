ca6e72e946521e6cbae16e25f27fff473a3e78bc
hayul090615 <2211007@shindo.ms.kr>
2026-03-24 20:17:59 +0900

    println
    첫 자바 프린트 출력했습니다.

---
+++ Cookie1.java
@@ -0,0 +1,32 @@
+package exam;
+
+public class Cookie1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕 김하율");
+
+        System.out.println("출력문");
+        System.out.println(1);
+        System.out.println("1");
+        System.out.println(1+1);
+        System.out.println("1"+"1");
+        System.out.println("1"+1);
+        System.out.println(1*1);
+
+        System.out.println("\\");
+        System.out.println("nnn \n nnn");
+        System.out.println("ttt \n ttt");
+        System.out.println("' ' ");
+        System.out.println("\""+"\' ");
+
+        System.out.print("\t\n");
+        System.out.print("현\t덕\n");
+        System.out.print("현\t덕\n");
+        System.out.print("이현\t덕\n");
+
+        System.out.printf("%d",1);
+        System.out.printf("\n%c",'d');
+	}
+
+}
