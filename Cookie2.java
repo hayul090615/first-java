fe81be46af666b6f15cf35ffbd67ccbed4653adc
hayul090615 <2211007@shindo.ms.kr>
2026-03-25 21:42:08 +0900

    calculate
    계산함 실수 더블형

---
+++ Cookie2.java
@@ -0,0 +1,49 @@
+package exam;
+
+public class Cookie2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 0, y = 90;
+		
+		System.out.println(x + y);
+		char grade = 'A'; 
+        double rate = 0.0001;
+        System.out.printf("%.4f", rate);
+        
+        int a = 1;
+        System.out.print(a++);
+        
+        int i;
+        double f;
+        f = 5/4;
+        System.out.println(f);
+        f = (double)5 /4;
+        System.out.println(f);
+        f = (float)4 / (float)3;
+        System.out.println(f);
+        i = (int)1.3 + (int)1.8;
+        System.out.println(i);
+		
+        
+		System.out.println(8.0f/7);
+        
+		
+		double value1 = 12.23;
+		double value2 = 34.45;
+		System.out.println(value1+value2);
+		
+		
+		int num1 = 7;
+		int num2, num3;
+		
+		num2 = ++num1;
+		System.out.println(num1);
+		System.out.println(num2);
+
+		num3 = --num1;
+		System.out.println(num1);
+		System.out.println(num3);
+	}
+
+}
