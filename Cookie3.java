097a72b1ea570bf5c21032a4edcb8674d92b2713
hayul090615 <2211007@shindo.ms.kr>
2026-03-25 21:46:45 +0900

    input
    입력

---
+++ Cookie3.java
@@ -0,0 +1,31 @@
+package exam;
+
+public class Cookie3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0, y = 0; // x와 y를 모두 0으로 초기화
+
+        char grade = 'A'; // 문자 A를 grade에 대입 ''수정
+
+        double rate = 0.001; // rate에 0.001을 대입
+        int salary = 2000000; // salary에 2,000,000을 대입
+
+        byte n = 125; // n에 1000을 대입 범위 넘어감
+		
+		
+		
+		// int x = 0; 초기화 추가 해야함 
+		// System.out.println(x);
+		
+		double f = 80.0; //실수형이 안전함 5말고 5.0으로 표현해야함
+		System.out.println(5.0/9.0*(f-32.0));
+
+		
+		//float f = 1199.80f;
+		
+		long num = 2147863545L;
+		System.out.println(num);
+	}
+
+}
