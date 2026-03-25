20b3ddddc5c6dbc8a0b11ba493278ba98147319d
hayul090615 <2211007@shindo.ms.kr>
2026-03-25 21:48:58 +0900

    if
    조건문

---
+++ Cookie4.java
@@ -0,0 +1,16 @@
+package exam;
+
+public class Cookie4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int A = 70, B = 80, C = 40;
+		
+		if (A > B && B > C) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
+
+}
